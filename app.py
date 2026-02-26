830354c7928c7bdf38ea79ce4f67ec146acf77b6
Sanjeevkumar90 <happyjeev.forever@gmail.com>
2026-02-26 20:40:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.llms import HuggingFaceHub
 
 st.set_page_config(page_title="PDF Chatbot", layout="wide")
@@ -59,3 +59,4 @@ if query:
         result = qa.run(query)
         st.write("### 📌 Answer:")
         st.write(result)
+
